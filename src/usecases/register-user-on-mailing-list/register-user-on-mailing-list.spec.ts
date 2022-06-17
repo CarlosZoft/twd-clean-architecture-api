2a9058c311ef70fa5da42d214dec22a0402c8f7f
CarlosZoft <carlosk2gunn@hotmail.com>
2022-06-17 00:15:41 -0300

    🎨 fix: lint rules

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -3,5 +3,6 @@ import { UserData } from './user-data'
 describe('Register user on mailing list use case', () => {
   it('should add user with complete data to mailing list', async () => {
     const users: UserData[] = []
+    console.log(users)
   })
 })
