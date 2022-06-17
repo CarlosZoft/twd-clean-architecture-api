360ac892a63d13c2ccd1e1e61ac499f6151a833c
CarlosZoft <carlosk2gunn@hotmail.com>
2022-06-17 00:12:15 -0300

    ✨ feat: user data interface

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,7 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  it('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+  })
+})
