5fdc19f567e89b629e46302cee15d7416c852f80
CarlosZoft <carlosk2gunn@hotmail.com>
2022-06-23 23:53:47 -0300

    🎨 fix: format code according lint rules

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,7 +1,7 @@
-import { UserData } from "../user-data"
+import { UserData } from '../user-data'
 import { InMemoryUserRepository } from './in-memory-user-repository'
 
-describe("In Memory User Repository", () => {
+describe('In Memory User Repository', () => {
   test('should return null if user is not found', async () => {
     const users: UserData[] = []
     const userRepo = new InMemoryUserRepository(users)
--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -1,16 +1,15 @@
-import { UserRepository } from "../ports/user-repository";
-import { UserData } from "../user-data";
+import { UserRepository } from '../ports/user-repository'
+import { UserData } from '../user-data'
 
 export class InMemoryUserRepository implements UserRepository {
-
   private repository: UserData[]
 
-  constructor(repository: UserData[]) {
+  constructor (repository: UserData[]) {
     this.repository = repository
   }
 
-  add(user: UserData): Promise<void> {
-    throw new Error("Method not implemented.");
+  add (user: UserData): Promise<void> {
+    throw new Error('Method not implemented.')
   }
 
   findUserByEmail (email: string): Promise<UserData> | null {
@@ -18,11 +17,10 @@ export class InMemoryUserRepository implements UserRepository {
   }
 
   findAllUsers (): Promise<UserData[]> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
 
   exists (user: UserData): Promise<boolean> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
-
 }
