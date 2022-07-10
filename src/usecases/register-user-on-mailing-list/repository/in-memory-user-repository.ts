9a2bf7185a468ba2453f4952aa9a142fbf2b4d3b
CarlosZoft <carlosk2gunn@hotmail.com>
2022-07-09 21:21:12 -0300

    ✨ feat: add/find user for checks its exists

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -1,26 +1,37 @@
-import { UserRepository } from '../ports/user-repository'
-import { UserData } from '../user-data'
+import { UserRepository } from "../ports/user-repository";
+import { UserData } from "../user-data";
 
 export class InMemoryUserRepository implements UserRepository {
-  private repository: UserData[]
+  private repository: UserData[];
 
-  constructor (repository: UserData[]) {
-    this.repository = repository
+  constructor(repository: UserData[]) {
+    this.repository = repository;
   }
 
-  add (user: UserData): Promise<void> {
-    throw new Error('Method not implemented.')
+  async add(user: UserData): Promise<void> {
+    const alreadyExists = await this.exists(user);
+    if (alreadyExists) {
+      throw new Error("User already exists");
+    }
+    this.repository.push(user);
   }
 
-  findUserByEmail (email: string): Promise<UserData> | null {
-    return null
+  findUserByEmail(email: string): UserData | null {
+    const user = this.repository.find((user) => user.email === email);
+    if (!user) {
+      return null;
+    }
+    return user;
   }
 
-  findAllUsers (): Promise<UserData[]> {
-    throw new Error('Method not implemented.')
+  findAllUsers(): Promise<UserData[]> {
+    throw new Error("Method not implemented.");
   }
 
-  exists (user: UserData): Promise<boolean> {
-    throw new Error('Method not implemented.')
+  async exists(user: UserData): Promise<boolean> {
+    if (!(await this.findUserByEmail(user.email))) {
+      return false;
+    }
+    return true;
   }
 }
