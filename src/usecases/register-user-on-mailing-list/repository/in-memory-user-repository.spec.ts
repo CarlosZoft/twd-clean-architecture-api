4ff00c002d4f02a6fdd417d82e27ed8ee2144724
CarlosZoft <carlosk2gunn@hotmail.com>
2022-07-09 21:21:29 -0300

    ✅ test: add/find user for checks its exists

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,11 +1,20 @@
-import { UserData } from '../user-data'
-import { InMemoryUserRepository } from './in-memory-user-repository'
+import { UserData } from "../user-data";
+import { InMemoryUserRepository } from "./in-memory-user-repository";
 
-describe('In Memory User Repository', () => {
-  test('should return null if user is not found', async () => {
-    const users: UserData[] = []
-    const userRepo = new InMemoryUserRepository(users)
-    const user = await userRepo.findUserByEmail('any@email.com')
-    expect(user).toBeNull()
-  })
-})
+describe("In Memory User Repository", () => {
+  test("should return null if user is not found", async () => {
+    const users: UserData[] = [];
+    const userRepo = new InMemoryUserRepository(users);
+    const user = await userRepo.findUserByEmail("any@email.com");
+    expect(user).toBeNull();
+  });
+
+  test("should return user if it is found in the repository", async () => {
+    const users: UserData[] = [];
+    const name = "any_name";
+    const email = "any@email.com";
+    const userRepo = new InMemoryUserRepository(users);
+    await userRepo.add({ name, email });
+    expect(users[0].name).toBe("any_name");
+  });
+});
